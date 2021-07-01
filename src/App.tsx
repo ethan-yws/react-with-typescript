5d2b0e2d7990ded51440aa16b48aaedbd26a6612
ethan-yws <38035032+ethan-yws@users.noreply.github.com>
2021-07-01 17:43:23 +1000

    react w/ ts demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import List from "./components/List";
+import AddToList from "./components/AddToList";
+
+export interface IState {
+    people: {
+        name: string;
+        age: number;
+        url: string;
+        note?: string;
+    }[];
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [people, setPeople] = useState<IState["people"]>([
+        {
+            name: "LeBron James",
+            age: 35,
+            url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/LeBron_James_crop.jpg/330px-LeBron_James_crop.jpg",
+            note: "Need KD!",
+        },
+    ]);
+
+    return (
+        <div className="App">
+            <h1>People invited to my Party</h1>
+            <List people={people} />
+            <AddToList people={people} setPeople={setPeople} />
+        </div>
+    );
 }
 
 export default App;
